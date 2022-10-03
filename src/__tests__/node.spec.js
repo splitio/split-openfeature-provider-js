3b91d114bc0d0da9e9a234f1714dcd084c6ca211
Josh Sirota <josh.sirota@split.io>
2022-10-03 12:09:21 -0700

    Remove extra unimplemented test suite

--- src/__tests__/node.spec.js
+++ src/__tests__/node.spec.js
@@ -1,8 +1,7 @@
 import tape from 'tape-catch';
 
 import clientSuite from './nodeSuites/client.spec.js';
-import providerSuite from './nodeSuites/provider.spec.js';
 
-tape('## OpenFeature NodeJS Split Provider - tests', async function (assert) {
+tape('## OpenFeature JavaScript Split Provider - tests', async function (assert) {
   assert.test('Client Tests', clientSuite);
 });
